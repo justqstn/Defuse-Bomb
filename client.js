a26be56b710d038a72d70c09eacdc0cf4f2e6649
justqstn <128418414+justqstn@users.noreply.github.com>
2023-08-11 11:03:07 +0300

    Update client.js

--- client.js
+++ client.js
@@ -97,13 +97,13 @@ Teams.OnPlayerChangeTeam.Add(function (p) {
 	if (p.Team != null) {
 		msg.Show(true);
 		p.Properties.Get("team").Value = p.Team.Id;
-	} else msg.Show(false);
-});
-
-Players.OnPlayerDisconnected.Add(function (p) {
-	try {
-		p.Properties.Deaths.Value++;
-	} catch(e) { msg.Show(e.name + " " + e.message); }
+	} else {
+		msg.Show(p.Properties.Get("team").Value);
+		let tm = Teams.Get(c.Player.Properties.Get("team").Value);
+		tm.Properties.Get("plrs").Value--;
+		if (!is_planted.Value && tm.Properties.Get("plrs").Value <= 0) EndRound(AnotherTeam(tm));
+		if (tm == ct_team && is_planted.Value && tm.Properties.Get("plrs").Value <= 0) EndRound(t_team);
+	}
 });
 
 Damage.OnDeath.Add(function (p) {
