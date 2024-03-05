efd1c3e2f57702b07050a5130d56d49da4149eb8
justqstn <128418414+justqstn@users.noreply.github.com>
2024-03-06 00:15:09 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -145,7 +145,17 @@ API.Teams.OnPlayerChangeTeam.Add(function (p) {
     }
 });
 
-
+API.Players.OnPlayerConnected.Add(function (p) {
+    if (Blacklist.Value.search(p.Id) != -1) {
+        p.Spawns.Spawn();
+        p.Spawns.Despawn();
+        p.Properties.Get("banned").Value = true;
+    }
+    else {
+        p.Properties.Get("banned").Value = false;
+    }
+    JQUtils.SetTimeout(JoinToTeam, 5, p);
+});
 
 
 // Функции
